6933bed532acfb925ce0b208a23d46172ef7f3d3
Julián Abrego <abrego.jul@gmail.com>
2025-04-17 19:20:57 -0300

    fix: styles footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="fixed bottom-0 h-16 w-full bg-secondary text-primary text-sm flex justify-center items-center">
+      © 2025 Digital Money House
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,9 +4,9 @@ import Button from "../common/Button";
 
 const Header = () => {
   return (
-    <nav className="h-16 w-full bg-secondary">
+    <nav className="fixed top-0 h-16 w-full bg-background">
       <div className="flex justify-between items-center h-full w-full px-4">
-        <Image src="/images/logo-01-primary.png" alt="Digital Money House" width={83} height={33} />
+        <Image src="/images/logo-01-primary.png" alt="Digital Money House" width={83} height={33} className="md:w-[83px] w-[63px]" />
         <div className="flex gap-4">
           <Button mode={'secondary'} size={"small"}>Ingresar</Button>
           <Button mode={'primary'} size={"small"}>Crear cuenta</Button>
