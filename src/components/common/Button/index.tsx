5dfee716d593dc22be6fea70902584e380d09187
Julián Abrego <abrego.jul@gmail.com>
2025-04-17 19:08:07 -0300

    feat: adds and styles Button component

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  mode = "primary",
+  size = "medium",
+}) => {
+  return (
+    <button className={`button-${mode} button-${size}`}>{children}</button>
+  );
+};
+
+type ButtonProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  mode?: "primary" | "secondary";
+  size?: "small" | "medium" | "large";
+};
+
+export default Button;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import Button from "../common/Button";
 
 const Header = () => {
   return (
     <nav className="h-16 w-full bg-secondary">
       <div className="flex justify-between items-center h-full w-full px-4">
         <Image src="/images/logo-01-primary.png" alt="Digital Money House" width={83} height={33} />
-        <div>
-          <button className="mx-2">Button 1</button>
-          <button className="mx-2">Button 2</button>
+        <div className="flex gap-4">
+          <Button mode={'secondary'} size={"small"}>Ingresar</Button>
+          <Button mode={'primary'} size={"small"}>Crear cuenta</Button>
         </div>
       </div>
     </nav>
